716ec32360bdd074549f02351af90fd0720d9a92
Tan Eu Zin <tan.euzin@bytedance.com>
2024-10-01 23:19:38 +0800

    feature: implement handleRegister

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,16 +1,20 @@
 package user
 
 import (
+	"ecom/types"
+	"ecom/utils"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -23,5 +27,36 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+
+	if err := utils.ParseJSON(r, payload); err == nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user wilth email %s already exists", payload.Email))
+		return
+	}
+
+	hashedPassword := ""
+
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
+
+	// check if user exists
+	// if no exists create new user
 
 }
