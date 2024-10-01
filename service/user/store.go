dec4975026a3f3c998ce26b46096058bc581fe22
Tan Eu Zin <tan.euzin@bytedance.com>
2024-10-01 23:19:23 +0800

    feature: create new user store

---
+++ service/user/store.go
@@ -0,0 +1,64 @@
+package user
+
+import (
+	"database/sql"
+	"ecom/types"
+	"fmt"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROME users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
+}
+
+func (s *Store) GetUserByID(ID int) (*types.User, error) {
+	u := new(types.User)
+	return u, nil
+}
+
+func (s *Store) CreateUser(types.User) error {
+	return nil
+}
+
+func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+
+	err := rows.Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, err
+}
